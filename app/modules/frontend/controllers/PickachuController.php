2d27de02e3a3b6fb22b067ecffc873dc9ff2c341
Isaia91 <isaia.maperi@gmail.com>
2023-07-11 16:29:21 +1100

    first commit

---
+++ app/modules/frontend/controllers/PickachuController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Test1\modules\frontend\controllers;
+
+class PickachuController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
