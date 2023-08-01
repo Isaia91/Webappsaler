2e413b21d885faa48f3dd3de08513e68d3c4f7e0
Isaia91 <isaia.maperi@gmail.com>
2023-08-01 13:39:01 +1100

    modification du controller equipes pour afficher les membres de l'equipe

--- app/modules/frontend/controllers/EquipeController.php
+++ app/modules/frontend/controllers/EquipeController.php
@@ -11,8 +11,55 @@ class EquipeController extends \Phalcon\Mvc\Controller
 
     public function indexAction( )
     {
-        $equipes = [];
+
+        $nomequipes = [];
         foreach (Equipe::find() as $equipe) {
+            $nomequipes [] = [
+                'id' => $equipe->getId(),
+                'nom'=>$equipe->getNom(),
+                'cdp'=>$equipe->getChefDeProjetId(),
+                'nomcdp'=>$equipe->Chefdeprojet->Collaborateur->getPrenomNom()
+            ];
+            }
+
+        $equipesHtml = "";
+        foreach ($nomequipes as $nomequipe) {
+            $equipesHtml .= "<h2>" . $nomequipe['nom'] ."</h2>";
+            $equipesHtml .= "<h4>" .  " Chef de projet : ".$nomequipe['nomcdp']."</h4>";
+            $equipesHtml .= "<table class='table'>
+                        <thead>
+                            <tr>
+                                <th scope='col'>id</th>
+                                <th scope='col'>Nom Prenom</th>
+                            </tr>
+                        </thead>
+                        <tbody>";
+
+            foreach (Equipe::find(['conditions' => 'id =' . $nomequipe['id']]) as $equipe) {
+
+                $equipeMembers = $equipe->getEquipeMembres();
+                foreach ($equipeMembers as $equipeMember) {
+                    $equipesHtml .= "<tr>";
+                    $equipesHtml .= "<td>" . $equipeMember->Developpeur->Collaborateur->getId() . "</td>";
+                    $equipesHtml .= "<td>" . $equipeMember->Developpeur->Collaborateur->getPrenomNom() . "</td>";
+                    $equipesHtml .= "</tr>";
+                }
+            }
+            $equipesHtml .= "</tbody>";
+            $equipesHtml .= "</table>";
+            $equipesHtml .= "<br>";
+        }
+
+        /*
+        foreach (Equipe::find() as $equipe) {
+            $equipesHtml.="<tr>";
+            $equipesHtml.="<td>".$equipe->getId()."</td>";
+            $equipesHtml.="<td>".$equipe->getChefDeProjetId()."</td>";
+            $equipesHtml.="<td>".$equipe->getNom()."</td>";
+            $equipesHtml.="<td>".$equipe->Chefdeprojet->Collaborateur->getPrenomNom()."</td>";
+            $equipesHtml.="</tr>";
+
+
             $equipes [] = [
                 'id' => $equipe->getId(),
                 'chefDeProjet_id'=>$equipe->getChefDeProjetId(),
@@ -20,8 +67,9 @@ class EquipeController extends \Phalcon\Mvc\Controller
                 'cdp' => $equipe->Chefdeprojet->Collaborateur->getPrenomNom()
             ];
 
-        }
-        $this->view->setVar('equipes', $equipes);
+        }*/
+        $equipesHtml.="</table>";
+        $this->view->setVar('equipesHtml', $equipesHtml);
     }
 
 }
